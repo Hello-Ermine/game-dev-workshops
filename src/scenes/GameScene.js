341c9bf9e5eb2e8bff4aadcf1b85f92a602f542a
JoNoteJoMaMa <notnot45@hotmail.com>
2021-10-25 19:52:57 +0700

    เห้ยยย นี่มันเกมนี่หว่าาาา
    Sadcat

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 
+let bg;
+let player;
+
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -8,15 +11,21 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('bg','src/image/tileSprite.png');
+        this.load.image('player','src/image/S A D C A T.jpg');
     }
 
     create() {
-        
+        bg = this.add.tileSprite(0,0,450,720,'bg');
+        bg.setOrigin(0,0);
+        bg.setScale(2);
+
+        player = this.add.sprite(225,350,'player');
+        player.setScale(0.3);
     }
 
     update(delta, time) {
-        
+        bg.tilePositionY -= 1;
     }
 }
 export default GameScene;
