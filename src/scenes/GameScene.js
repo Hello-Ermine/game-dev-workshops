5ffec3f46e01337671fd19840422ac2863e151cc
Chanpol Thippawan <chanpol.thip@kmutt.ac.th>
2021-10-25 19:52:04 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 
+let bg;
+let player;
+
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -8,15 +11,21 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('bg','src/image/tileSprite.png');
+        this.load.image('player','src/image/Pol.jpg');
     }
 
     create() {
-        
+        bg = this.add.tileSprite(0,0,450,720,'bg');
+        bg.setOrigin(0,0);
+        bg.setScale(2);
+
+        player = this.add.sprite(220,350,'player');
+        player.setScale(0.3);
     }
 
     update(delta, time) {
-        
+        bg.tilePositionY -= 1;
     }
 }
 export default GameScene;
