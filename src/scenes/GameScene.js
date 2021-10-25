1eae4082c28e22af3eb7bba0c046c207bf5aa635
Supakorn Chat-anothai <supakorn.alexis@mail.kmutt.ac.th>
2021-10-25 19:52:02 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 
+let bg;
+let player;
+
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -8,14 +11,22 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('bg', 'src/image/tileSprite.png');
+        this.load.image('player', 'src/image/playerBoy.png');
     }
 
     create() {
-        
+       bg =  this.add.tileSprite(0,0,450,720,'bg');
+       bg.setOrigin(0,0);
+       bg.setScale(3);
+
+       player = this.add.sprite(225, 360, 'player');
+       player.setScale(2);
+
     }
 
     update(delta, time) {
+        bg.tilePositionY += 1;
         
     }
 }
