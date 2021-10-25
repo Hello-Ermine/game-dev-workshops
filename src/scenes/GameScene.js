a8854d95e6b0f3b22c5aca170e6fde83d0930e80
Boss <c3bosskung@gmail.com>
2021-10-25 19:52:29 +0700

    Update GameScene.js
    add bg and player

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 
+let background;
+let player;
+
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -8,15 +11,22 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('background', 'src/image/tileSprite.png');
+        this.load.image('player', 'src/image/Boss.jpg');
     }
 
     create() {
+        background = this.add.tileSprite(0, 0, 450, 720, 'background');
+        background.setOrigin(0,0);
+        background.setScale(1.88);
+
+        player = this.add.sprite(226, 350, 'player');
+        player.setScale(0.2);
         
     }
 
     update(delta, time) {
-        
+        background.tilePositionY -= 0.5;
     }
 }
 export default GameScene;
