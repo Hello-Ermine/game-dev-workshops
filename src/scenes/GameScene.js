fb61877c1186d6fc821d2f35d5093b2bbb3bead7
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2021-10-25 21:12:32 +0700

    GameScene.js
    player = Crimson

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,4 +1,6 @@
 import Phaser from "phaser";
+let bg;
+let player;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -8,15 +10,21 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('bg', 'src/image/titleSprite.png');
+        this.load.image('player', 'src/image/Crimson.jpg');
     }
 
     create() {
-        
+        bg = this.add.titleSprite(0,0,450,720,'bg');
+        bg.setOrigin(0.0);
+        bg.setScale(2);
+
+        player = this.add.sprite(220,350,'player');
+        player.setScale(0.5);
     }
 
     update(delta, time) {
-        
+        bg.titlePositionY -= 1;
     }
 }
 export default GameScene;
