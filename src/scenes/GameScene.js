4344ef0bc581cec8ad878db20eaad71b8119c711
Boss <c3bosskung@gmail.com>
2021-10-30 15:57:21 +0700

    add animation

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 
 let background;
 let player;
+let ermine;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -15,8 +16,15 @@ class GameScene extends Phaser.Scene {
         // this.load.image('player', 'src/image/Boss.jpg');
 
         this.load.image('bg', 'src/image/objectClass/backGround.png');
-        this.load.spritesheet('player', 'src/image/objectClass/playerErmine.png',
-                                { frameWidth: 500, frameHeight: 300 });
+
+        // this.load.image('football', 'src/image/objectClass/football.png');
+        // this.load.image('basketball', 'src/image/objectClass/basketball.png');
+        // this.load.image('volleyball', 'src/image/objectClass/volleyball.png');
+        // this.load.image('takraw', 'src/image/objectClass/Takraw.png');
+
+        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png',
+            { frameWidth: 500, frameHeight: 300 });
+
 
     }
 
@@ -28,16 +36,34 @@ class GameScene extends Phaser.Scene {
         // player = this.add.sprite(226, 350, 'player');
         // player.setScale(0.2);
 
-        background = this.add.image(150, 150, 'bg');
-        background.setScale(1.6);
+        background = this.add.tileSprite(0, 0, 1280, 720, 'bg');
+        background.setOrigin(0, 0);
 
-        player = this.add.sprite(200, 200, 'player');
-        player.setScale(0.25)
+        player = this.physics.add.sprite(100, 450, 'ermine');
+        player.setScale(0.5);
+
+        ermine = this.anims.create({ 
+            key: 'ermineAni', 
+            frames: this.anims.generateFrameNumbers('ermine', { 
+                start: 0, 
+                end: 2 
+            }),
+            duration: 500, 
+            framerate: 0,
+            repeat: -1 
+        })
+
+        // ball = this.physics.add.group();
+        // ball.create(250, 250, 'football').setScale(0.1);
+        // ball.create(300, 300, 'basketball').setScale(0.1);
+        // ball.create(200, 200, 'volleyball').setScale(0.1);
+        // ball.create(350, 350, 'takraw').setScale(0.1);
+
     }
 
     update(delta, time) {
-        // background.tilePositionY -= 0.5;
-        
+        background.tilePositionX += 2;
+        player.anims.play('ermineAni', true);
     }
 }
 export default GameScene;
