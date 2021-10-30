2bb57d18b023a9174496c3718664ea7b105de424
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2021-10-30 14:09:24 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -13,21 +13,23 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg', 'src/image/titleSprite.png');
-        this.load.image('player', 'src/image/Crimson.jpg');
+        //this.load.image('bg', 'src/image/titleSprite.png');
+        //this.load.image('player', 'src/image/Crimson.jpg');
+        this.load.image('bg', 'src/image/objectClass/backGround.ng');
     }
 
     create() {
-        bg = this.add.titleSprite(0,0,450,720,'bg');
-        bg.setOrigin(0.0);
-        bg.setScale(2);
+        //bg = this.add.titleSprite(0,0,450,720,'bg');
+       // bg.setOrigin(0.0);
+        //bg.setScale(2);
 
-        player = this.add.sprite(220,350,'player');
-        player.setScale(0.5);
+        //player = this.add.sprite(220,350,'player');
+        //player.setScale(0.5);
+        backGround = this.add.image(150,150,'bg').setScale(1.75);
     }
 
     update(delta, time) {
-        bg.titlePositionY -= 1;
+       // bg.titlePositionY -= 1;
     }
 }
 export default GameScene;
