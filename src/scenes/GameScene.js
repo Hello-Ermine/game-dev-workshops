e3266f86838ab750372a29c7abbca84e30c3b917
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2021-10-30 16:46:16 +0700

    3 People Dancing
    now have playergirl

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
 
-let bg,player,playerboy;
+let bg,player,playerboy,playergirl;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -13,14 +13,19 @@ class GameScene extends Phaser.Scene {
         this.load.image('bg', 'src/image/objectClass/backGround.png');
         this.load.spritesheet('player', 'src/image/objectClass/playerErmine.png',{ frameWidth: 500, frameHeight: 300 });
         this.load.spritesheet('playerboy', 'src/image/playerBoy.png',{ frameWidth: 15, frameHeight: 21 });
+        this.load.spritesheet('playergirl', 'src/image/playerGirl.png',{ frameWidth: 17, frameHeight: 21 });
     }
 
     create() {  
         bg = this.add.tileSprite(0,0,450,720,'bg').setDepth(1);
         bg.setOrigin(0,0);
 
-        player = this.physics.add.sprite(60,500,'player').setDepth(10);
-        player.setScale(0.35);
+        //Ermine
+        player = this.physics.add.sprite(100,500,'player')
+            .setDepth(10)
+            .setScale(0.35)
+            .setSize(300, 100)
+            .setOffset(150, 140);
         this.anims.create({
             key: 'erminerun',
             frames: this.anims.generateFrameNumbers('player', {
@@ -30,9 +35,12 @@ class GameScene extends Phaser.Scene {
             duration: 90,
             framerate: 0,
             repeat: -1 })
-        
-        playerboy = this.physics.add.sprite(80,420,'playerboy').setDepth(10);
-        playerboy.setScale(4);
+        //BOY
+        playerboy = this.physics.add.sprite(150,420,'playerboy')
+            .setDepth(10)
+            .setScale(4)
+            .setSize(12,20)
+            .setOffset(1,1);
         this.anims.create({
             key: 'boyrun',
             frames: this.anims.generateFrameNumbers('playerboy', {
@@ -42,11 +50,29 @@ class GameScene extends Phaser.Scene {
             duration: 200,
             framerate: 0,
             repeat: -1 })
+        
+        //GIRL
+        playergirl = this.physics.add.sprite(60,420,'playergirl')
+            .setDepth(10)
+            .setScale(4)
+            .setSize(12,20)
+            .setOffset(2,1);
+        this.anims.create({
+            key: 'girlrun',
+            frames: this.anims.generateFrameNumbers('playergirl', {
+                start: 0,
+                end: 8
+            }),
+            duration: 200,
+            framerate: 0,
+            repeat: -1 })
         }
+
     update(delta, time) {
         bg.tilePositionX += 12;
         player.anims.play('erminerun', true);
         playerboy.anims.play('boyrun', true);
+        playergirl.anims.play('girlrun', true);
     }
 }
 export default GameScene;
