808908b1fef08de72992055fb2b92b0bccb42c68
Mxoxoxk <Parisara.mxoxoxk@mail.kmutt.ac.th>
2021-10-30 16:34:45 +0700

    Ermine Animation

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,8 @@
 import Phaser from "phaser";
 
-let bg;
-let player;
+let background;
+let ermine;
+let ball;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -11,21 +12,54 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg','src/image/tileSprite.png');
-        this.load.image('player','src/image/playerDog.jpg');
+        // load image
+        this.load.image('bg','src/image/objectClass/backGround.png');
+        this.load.image('football','src/image/objectClass/football.png');
+        this.load.image('basketball','src/image/objectClass/basketball.png');
+        this.load.image('snowball','src/image/objectClass/snowball.png');
+        this.load.image('Takraw','src/image/objectClass/Takraw.png');
+        this.load.image('volleyball','src/image/objectClass/volleyball.png');
+        // load spritesheet
+        this.load.spritesheet('ermine','src/image/objectClass/playerErmine.png',
+                                        {frameWidth:500,frameHeight:300});
+        //load tileSprite
     }
 
     create() {
-       bg = this.add.tileSprite(0,0,450,720,'bg');
-       bg.setOrigin(0,0);
-       bg.setScale(2);
+        // create image object (background)
+        //background = this.add.image(150,150,'bg').setScale(1.75);
+        //create tileSprite
+        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+        // create sprite object (player)
+        ermine = this.physics.add.sprite(250,450,'ermine');
+        ermine.setScale(0.5);
+        
+        this.anims.create({
+            key: 'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine',{
+                start: 0,
+                end:2
+            }),
+            duration:750,
+            framerate: 0,
+            repeat: -1
+        })
 
-       player = this.add.sprite(220,350,'player');
-       player.setScale(0.5);
+        ermine.anims.play('ermineAni',true);
+        
+        //create group obj
+        //ball = this.physics.add.group()
+        //ball.create(250,250,'football');
+        //ball.create(250,250,'basketball');
+        //ball.create(250,250,'snowball');
+        //ball.create(250,250,'Takraw');
+        //ball.create(250,250,'volleyball');
+        
     }
 
     update(delta, time) {
-        bg.tilePositionY -= 2;
+        background.tilePositionX += 3;
+        
     }
 }
 export default GameScene;
