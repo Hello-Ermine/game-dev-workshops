ba9ab6d889e5c8397f8d3e59070c71e533595755
Chanpol Thippawan <chanpol.thip@kmutt.ac.th>
2021-10-30 16:28:10 +0700

    Animation

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 
-let bg;
-let player;
+let background;
+let ermine;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -11,21 +11,33 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg','src/image/tileSprite.png');
-        this.load.image('player','src/image/Pol.jpg');
+        this.load.image('bg','src/image/objectClass/backGround.png')
+        //load spritesheet object (player)
+        this.load.spritesheet('ermine','src/image/objectClass/playerErmine.png',
+            {frameWidth:500,frameHeight:300});
     }
 
     create() {
-        bg = this.add.tileSprite(0,0,450,720,'bg');
-        bg.setOrigin(0,0);
-        bg.setScale(2);
-
-        player = this.add.sprite(220,350,'player');
-        player.setScale(0.3);
+        //create image object (background)
+        background = this.add.tileSprite(0,0,850,1400,'bg').setOrigin(0,0);
+        //create sprite object (player)
+        ermine = this.physics.add.sprite(200,500,'ermine').setScale(0.5);
+        
+        this.anims.create({
+            key:'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine',{
+                start: 0,
+                end: 2
+            }),
+            duration: 200,
+            repeat: -1
+        });
+        ermine.anims.play('ermineAni',true);
     }
 
     update(delta, time) {
-        bg.tilePositionY -= 1;
+        background.tilePositionX += 15;
+        
     }
 }
 export default GameScene;
