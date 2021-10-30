84d229efbb473fcbf467d4dd36d57488402347d6
sarinweerakun <sarin.kobfha@mail.kmutt.ac.th>
2021-10-30 14:36:11 +0700

    tileSprite

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -16,18 +16,22 @@ class GameScene extends Phaser.Scene {
         // load spritesheet
         this.load.spritesheet('player','src/image/objectClass/playerErmine.png',
                                         {frameWidth:500,frameHeight:300});
+        // load tileSprite
+        
     }
 
     create() {
         // create image object (background)
-        background = this.add.image(150,150,'bg').setScale(1.75);
+        // background = this.add.image(150,150,'bg').setScale(1.75);
         // create sprite object (player)
         player = this.add.sprite(200,200,'player');
         player.setScale(0.5);
+        // create tileSprite
+        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
     }
 
     update(delta, time) {
-        
+        background.tilePositionX += 2;
     }
 }
 export default GameScene;
