9568f2e1015f3c7d802d97e535ec7d7c93850330
thannichasompan <thannicha.som@mail.kmutt.ac.th>
2021-10-30 16:04:53 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 
 let bg;
-let player;
+let ermine;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -14,7 +14,7 @@ class GameScene extends Phaser.Scene {
         // this.load.image('bg', 'src/image/tileSprite.png');
         // this.load.image('player', 'src/image/playermeow.jpg');
         this.load.image('bg', 'src/image/objectClass/backGround.png');
-        this.load.spritasheet('player', 'src/image/objectClass/playerErmine.png',
+        this.load.spritasheet('ermine', 'src/image/objectClass/playerErmine.png',
                                             {frameWidth:500,frameHeight:300});
 
     }
@@ -23,18 +23,28 @@ class GameScene extends Phaser.Scene {
         //bg = this.add.tileSprite(0,0,450,720,'bg');
         //bg.setOrigin(0,0);
         //bg.setScale(2);
-
         // player = this.add.sprite(220,350,'player');
         // player.setScale(0.5);
+        background = this.add.tileSprite(0,0,450, 150, 'bg').setScale(1.75).setDepth(2);;
+        ermine = this.physics.add.sprite(200,200,'player');
+        ermine.setScale(0.5).setDepth(5);;
 
-        background = this.add.tileSprite(0,0,450, 150, 'bg').setScale(1.75);
-        player = this.add.sprite(200,200,'player');
-        player.setScale(0.25);
+        this.anims.create({
+            key: 'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine', {
+                start: 0,
+                end: 2
+            }),
+            duration: 1000,
+            repeat: -1
+        })
     }
-
     update(delta, time) {
         // bg.tilePositionY -= 1;
-        background.tilePositionX -=5;
+        //background.tilePositionX -=5;
+
+        ermine.anims.play('ermineAni', true);
+
     }
 }
 export default GameScene;
