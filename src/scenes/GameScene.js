e4e2fac34b105a38742059e6a2ac4fae0d39baee
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2021-10-30 14:34:38 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -16,20 +16,26 @@ class GameScene extends Phaser.Scene {
         //this.load.image('bg', 'src/image/titleSprite.png');
         //this.load.image('player', 'src/image/Crimson.jpg');
         this.load.image('bg', 'src/image/objectClass/backGround.ng');
+        this.load.spritSheet('player','src/image/objectClass/playerErmine.png',{frameWidth:500,frameHeight:300});
+        //this.load.image();
     }
 
     create() {
         //bg = this.add.titleSprite(0,0,450,720,'bg');
-       // bg.setOrigin(0.0);
+        //bg.setOrigin(0.0);
         //bg.setScale(2);
 
         //player = this.add.sprite(220,350,'player');
         //player.setScale(0.5);
-        backGround = this.add.image(150,150,'bg').setScale(1.75);
+        //backGround = this.add.image(150,150,'bg').setScale(1.75);
+        player = this.add.spite(200, 200, 'player');
+        player.setScale(0.5);
+        backGround = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
     }
 
     update(delta, time) {
        // bg.titlePositionY -= 1;
+       backGround.tilePositionX = -1;
     }
 }
 export default GameScene;
