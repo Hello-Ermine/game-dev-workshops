5c4717b6b8cd94de9bd158d654625d7a6207ad46
Boss <c3bosskung@gmail.com>
2021-10-30 14:23:45 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,21 +11,33 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'src/image/tileSprite.png');
-        this.load.image('player', 'src/image/Boss.jpg');
+        // this.load.image('background', 'src/image/tileSprite.png');
+        // this.load.image('player', 'src/image/Boss.jpg');
+
+        this.load.image('bg', 'src/image/objectClass/backGround.png');
+        this.load.spritesheet('player', 'src/image/objectClass/playerErmine.png',
+                                { frameWidth: 500, frameHeight: 300 });
+
     }
 
     create() {
-        background = this.add.tileSprite(0, 0, 450, 720, 'background');
-        background.setOrigin(0,0);
-        background.setScale(1.88);
+        // background = this.add.tileSprite(0, 0, 450, 720, 'background');
+        // background.setOrigin(0,0);
+        // background.setScale(1.88);
 
-        player = this.add.sprite(226, 350, 'player');
-        player.setScale(0.2);
+        // player = this.add.sprite(226, 350, 'player');
+        // player.setScale(0.2);
+
+        background = this.add.image(150, 150, 'bg');
+        background.setScale(1.6);
+
+        player = this.add.sprite(200, 200, 'player');
+        player.setScale(0.25)
     }
 
     update(delta, time) {
-        background.tilePositionY -= 0.5;
+        // background.tilePositionY -= 0.5;
+        
     }
 }
 export default GameScene;
