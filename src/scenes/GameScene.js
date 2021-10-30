85a9c70a5c7029e8697c36f81b5bd1430ea95a91
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2021-10-30 15:00:17 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,6 +4,7 @@ let player;
 
 let bg;
 let player;
+let ball;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -18,6 +19,10 @@ class GameScene extends Phaser.Scene {
         this.load.image('bg', 'src/image/objectClass/backGround.ng');
         this.load.spritSheet('player','src/image/objectClass/playerErmine.png',{frameWidth:500,frameHeight:300});
         //this.load.image();
+        this.load.image('football', 'src/image/objectClass/football.png');
+        this.load.image('football', 'src/image/objectClass/basketball.png');
+        this.load.image('football', 'src/image/objectClass/volleyball.png');
+        this.load.image('football', 'src/image/objectClass/Takraw.png');
     }
 
     create() {
@@ -30,7 +35,13 @@ class GameScene extends Phaser.Scene {
         //backGround = this.add.image(150,150,'bg').setScale(1.75);
         player = this.add.spite(200, 200, 'player');
         player.setScale(0.5);
-        backGround = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+        //backGround = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+        ball = this.physics.add.group()
+        ball.create(250,250,'football');
+        ball.create(300,300,'basket');
+        
+        ball.create(200,200,'volley');
+        ball.create(350,350,'takraw');
     }
 
     update(delta, time) {
