db42d173569b7cad9fece322057c88982b4283df
Nannapatx <nannapat.it@mail.kmutt.ac.th>
2021-10-30 16:42:20 +0700

    Workshop 30/10/2021
    Layer/Animate/Object

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,9 @@
 import Phaser from "phaser";
 
-let bg;
-let player;
+
+let background;
+let ermine;
+
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -11,22 +13,39 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg', 'src/image/tileSprite.png');
-        this.load.image('player', 'src/image/245917281_419542196422909_7107213039427024594_n.jpg');
+
+        this.load.image('bg', 'src/image/objectClass/backGround.png');
+        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png', { frameWidth: 500, frameHeight: 300 });
+
     }
 
     create() {
-        bg = this.add.tileSprite(0,0,450,720,'bg');
-        bg.setOrigin(0,0);
-        bg.setScale(2);
+       
 
-        player = this.add.sprite(225,500,'player');
-        player.setScale(0.5);
+        ermine = this.physics.add.sprite(100, 500, 'ermine').setScale(0.5).setDepth(99);
+        this.anims.create({
+            key: 'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine',{
+                start: 0,
+                end: 2
+            }),
+            duration: 1000,
+            framerate: 0,
+            repeat: -1
+        })
+
+        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+        ermine.anims.play('ermineAni',true)
+
+
+        
     }
 
     update(delta, time) {
-        bg.tilePositionY -= 1;
+        
+        background.tilePositionX += 2;
+
     }
 }
-export default GameScene; 
+export default GameScene;
 
