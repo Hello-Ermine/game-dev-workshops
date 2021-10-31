b404c9771db4e5fcf201ab253f388652ad8c865e
Boss <c3bosskung@gmail.com>
2021-10-31 08:04:12 +0700

    add Cute character

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,6 +3,7 @@ import Phaser from "phaser";
 let background;
 let ermine;
 let boom;
+let character;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -26,6 +27,8 @@ class GameScene extends Phaser.Scene {
             { frameWidth: 500, frameHeight: 300 });
         this.load.spritesheet('boom', 'src/image/Boom.png',
             { frameWidth: 105, frameHeight: 99 });
+        this.load.spritesheet('character', 'src/image/character.png',
+            { frameWidth: 51.75, frameHeight: 60.2 });
     }
 
     create() {
@@ -45,6 +48,10 @@ class GameScene extends Phaser.Scene {
         boom = this.physics.add.sprite(100, 450, 'boom');
         boom.setScale(3);
 
+        character = this.physics.add.sprite(500,450, 'character');
+        character.setScale(3)
+
+
         this.anims.create({ 
             key: 'ermineAni', 
             frames: this.anims.generateFrameNumbers('ermine', { 
@@ -62,13 +69,25 @@ class GameScene extends Phaser.Scene {
                 start: 0, 
                 end: 23
             }),
-            duration: 1500, 
+            duration: 383, 
+            framerate: 0,
+            repeat: -1
+        })
+
+        this.anims.create({ 
+            key: 'characterAni', 
+            frames: this.anims.generateFrameNumbers('character', { 
+                start: 0, 
+                end: 79
+            }),
+            duration: 7500, 
             framerate: 0,
             repeat: -1
         })
 
         ermine.anims.play('ermineAni', true);
         boom.anims.play('boomAni', true);
+        character.anims.play('characterAni', true);
 
         // ball = this.physics.add.group();
         // ball.create(250, 250, 'football').setScale(0.1);
