7ea7ba00c3bf03af5289b5cca35800fed3a1f422
janejumu <93077297+janejumu@users.noreply.github.com>
2021-10-31 15:20:50 +0700

    ลูกบอล

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,31 +1,48 @@
 import Phaser from "phaser";
 
-let bg;
+let background;
 let player;
+let ball;
 
 class GameScene extends Phaser.Scene {
-    constructor(test) {
-        super({
-            key: 'GameScene'
-        });
-    }
+   constructor(test) {
+       super({
+           key: 'GameScene'
+       });
+   }
 
-    preload() {
-        this.load.image('bg', 'src/image/tileSprite.png');
-        this.load.image('player', 'src/image/playermeow.jpg');
-    }
+   preload() {
+       // load image
+       this.load.image('bg','src/image/objectClass/backGround.png');
+       this.load.image('football','src/image/objectClass/football.png');
+       this.load.image('basket','src/image/objectClass/basketball.png');
+       this.load.image('volley','src/image/objectClass/volleyball.png');
+       this.load.image('takraw','src/image/objectClass/Takraw.png');
+       // load spritesheet
+       // this.load.spritesheet('player','src/image/objectClass/playerErmine.png',
+       //                                 {frameWidth:500,frameHeight:300});
+       // load tileSprite
 
-    create() {
-        bg = this.add.tileSprite(0,0,450,720,'bg');
-        bg.setOrigin(0,0);
-        bg.setScale(2);
+   }
 
-        player = this.add.sprite(220,350,'player');
-        player.setScale(0.5);
-    }
+   create() {
+       // create image object (background)
+       // background = this.add.image(150,150,'bg').setScale(1.75);
+       // create sprite object (player)
+       // player = this.add.sprite(200,200,'player');
+       // player.setScale(0.5);
+       // create tileSprite
+       // background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+       // create group obj
+       ball = this.physics.add.group();
+       ball.create(250,250,'football');
+       ball.create(300,300,'basket');
+       ball.create(200,200,'volley');
+       ball.create(350,350,'takraw');
+   }
 
-    update(delta, time) {
-        bg.tilePositionY -= 1;
-    }
+   update(delta, time) {
+       // background.tilePositionX += 2;
+   }
 }
 export default GameScene;
