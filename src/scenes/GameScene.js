504d6e40f667f50e985455cfe669f1ac8662936a
Chanpol Thippawan <chanpol.thip@kmutt.ac.th>
2021-11-01 20:01:25 +0700

    input&Scene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,13 @@
 import Phaser from "phaser";
+import { Pointer } from "phaser/src/input";
 
 let background;
 let ermine;
+let cursor;
+let keyW;
+let keyA;
+let keyS;
+let keyD;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -11,33 +17,74 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg','src/image/objectClass/backGround.png')
-        //load spritesheet object (player)
-        this.load.spritesheet('ermine','src/image/objectClass/playerErmine.png',
-            {frameWidth:500,frameHeight:300});
+        this.load.image('bg', 'src/image/objectClass/backGround.png');
+        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png',
+            { frameWidth: 500, frameHeight: 300 });
     }
 
     create() {
-        //create image object (background)
-        background = this.add.tileSprite(0,0,850,1400,'bg').setOrigin(0,0);
-        //create sprite object (player)
-        ermine = this.physics.add.sprite(200,500,'ermine').setScale(0.5);
+        background = this.add.tileSprite(0, 0, 850, 1400, 'bg').setOrigin(0, 0);
+        ermine = this.physics.add.sprite(200, 500, 'ermine').setScale(0.5);
         
         this.anims.create({
-            key:'ermineAni',
+            key: 'ermineAni',
             frames: this.anims.generateFrameNumbers('ermine',{
-                start: 0,
-                end: 2
+                start:0,
+                end:2
             }),
-            duration: 200,
-            repeat: -1
-        });
+            duration: 1000,
+            repeat:-1
+        })
         ermine.anims.play('ermineAni',true);
+
+        // this.input.on('pointermove', (pointer)=>{
+        //     ermine.x = pointer.x
+        //     ermine.y = pointer.y
+        // })
+
+        // cursor = this.input.keyboard.createCursorKeys();
+
+        ermine.setCollideWorldBounds(true)
+
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update(delta, time) {
-        background.tilePositionX += 15;
+        background.tilePositionX += 2;
         
+        // if(cursor.up.isDown){
+        //     ermine.setVelocityY(-500);
+        // }else if(cursor.down.isDown){
+        //     ermine.setVelocityY(500);
+        // }else{
+        //     ermine.setVelocityY(0);
+        // }
+        // if(cursor.left.isDown){
+        //     ermine.setVelocityX(-500);
+        // }else if(cursor.right.isDown){
+        //     ermine.setVelocityX(500);
+        // }else{
+        //     ermine.setVelocityX(0);
+
+        // }
+
+        if(keyW.isDown){
+            ermine.setVelocityY(-500);
+        }else if(keyS.isDown){
+            ermine.setVelocityY(500);
+        }else{
+            ermine.setVelocityY(0);
+        }
+        if(keyA.isDown){
+            ermine.setVelocityX(-500);
+        }else if(keyD.isDown){
+            ermine.setVelocityX(500);
+        }else{
+            ermine.setVelocityX(0);
+        }
     }
 }
 export default GameScene;
