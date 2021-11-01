9fc809f713b521c12aeff293552be5c90768cf72
thannichasompan <thannicha.som@mail.kmutt.ac.th>
2021-11-01 22:09:17 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 
+
 let background;
 let ermine;
 let cursor;
@@ -11,6 +12,18 @@ let keyD;
 
 
 class GameScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'GameScene'
+        });
+    }
+
+    preload() {
+
+        this.load.image('bg', 'src/image/objectClass/backGround.png');
+        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png', { frameWidth: 500, frameHeight: 300 });
+
+    }
 
     create() {
        
@@ -19,10 +32,15 @@ class GameScene extends Phaser.Scene {
         ermine = this.physics.add.sprite(100, 500, 'ermine').setScale(0.5).setDepth(99);
         this.anims.create({
             key: 'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine',{
+                start: 0,
+                end: 2
+            }),
+            duration: 1000,
+            framerate: 0,
             repeat: -1
         })
 
-        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
         ermine.setCollideWorldBounds(true)  
         ermine.anims.play('ermineAni',true)
 
