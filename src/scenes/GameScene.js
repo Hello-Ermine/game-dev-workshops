c5824096c2655043765b99fe9d8f2b9b411d9c61
thannichasompan <thannicha.som@mail.kmutt.ac.th>
2021-11-01 21:49:46 +0700

    ซอรี่นะแก เราแก้ผิดbranch เราแก้คืนให้นะ

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,5 @@
 import Phaser from "phaser";
 
-
 let background;
 let ermine;
 let cursor;
@@ -12,36 +11,21 @@ let keyD;
 
 
 class GameScene extends Phaser.Scene {
-    constructor(test) {
-        super({
-            key: 'GameScene'
-        });
-    }
-
-    preload() {
-
-        this.load.image('bg', 'src/image/objectClass/backGround.png');
-        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png', { frameWidth: 500, frameHeight: 300 });
-
-    }
 
     create() {
        
+
         background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
         ermine = this.physics.add.sprite(100, 500, 'ermine').setScale(0.5).setDepth(99);
         this.anims.create({
             key: 'ermineAni',
-            frames: this.anims.generateFrameNumbers('ermine',{
-                start: 0,
-                end: 2
-            }),
-            duration: 1000,
-            framerate: 0,
             repeat: -1
         })
 
+        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
         ermine.setCollideWorldBounds(true)  
         ermine.anims.play('ermineAni',true)
+
        
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -50,6 +34,7 @@ class GameScene extends Phaser.Scene {
 
         
     }
+
     update(delta, time) {
         
         if(keyW.isDown){
