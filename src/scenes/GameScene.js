667edfc7dabe0c7d2a056692cd2313d42340caa7
janejumu <93077297+janejumu@users.noreply.github.com>
2021-11-01 19:48:14 +0700

    Start + Ermine ขึ้นลง

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,11 +1,12 @@
 import Phaser from "phaser";
 
-<<<<<<< HEAD
 let background;
 let ermine;
-=======
-
->>>>>>> origin/input
+let cursor;
+let keyW;
+let keyA;
+let keyS;
+let keyD;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -15,14 +16,14 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-<<<<<<< HEAD
-        this.load.image('bg','src/image/objectClass/backGround.png');
+        this.load.image('bg', 'src/image/objectClass/backGround.png');
         this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png',
             { frameWidth: 500, frameHeight: 300 });
+
     }
 
     create() {
-        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+        background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0);
         ermine = this.physics.add.sprite(220, 500, 'ermine').setScale(0.5);
 
         this.anims.create({
@@ -35,23 +36,53 @@ class GameScene extends Phaser.Scene {
             repeat:-1
         })
         ermine.anims.play('ermineAni',true);
-=======
-        
 
-    }
+        this.input.on('pointermove', (pointer)=>{
+            ermine.x = pointer.x
+            ermine.y = pointer.y
+        })
 
-    create() {
-        
->>>>>>> origin/input
+        cursor = this.input.keyboard.createCursorKeys();
 
+        ermine.setCollideWorldBounds(true)
+
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update(delta, time) {
-<<<<<<< HEAD
-        background.tilePositionX += 2;
-=======
-        
->>>>>>> origin/input
+        background.tilePositionX += 4;
+        if(cursor.up.isDown){
+            ermine.setVelocityY(-500);
+        }else if(cursor.down.isDown){
+            ermine.setVelocityY(500);
+        }else{
+            ermine.setVelocityY(0);
+        }
+        if(cursor.left.isDown){
+            ermine.setVelocityX(-500);
+        }else if(cursor.right.isDown){
+            ermine.setVelocityX(500);
+        }else{
+            ermine.setVelocityX(0);
+        }
+
+        if(keyW.isDown){
+            ermine.setVelocityY(-500);
+        }else if(keyS.isDown){
+            ermine.setVelocityY(500);
+        }else{
+            ermine.setVelocityY(0);
+        }
+        if(keyA.isDown){
+            ermine.setVelocityX(-500);
+        }else if(keyD.isDown){
+            ermine.setVelocityX(500);
+        }else{
+            ermine.setVelocityX(0);
+        }
 
     }
 }
