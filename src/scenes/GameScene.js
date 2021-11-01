c7937f095efb2a6841ccae4e2c7a081da30eebfa
Mxoxoxk <Parisara.mxoxoxk@mail.kmutt.ac.th>
2021-11-01 20:05:02 +0700

    scenes

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,26 +2,90 @@ import Phaser from "phaser";
 
 
 
+
+let background;
+
+let ermine;
+
+//key
+let keyW;
+
+let keyA;
+
+let keyS;
+
+let keyD;
+
+
+
+
+
 class GameScene extends Phaser.Scene {
+
     constructor(test) {
+
         super({
+
             key: 'GameScene'
+
         });
+
     }
 
     preload() {
-        
+
+        this.load.image('bg', 'src/image/objectClass/backGround.png');
+
+        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png', { frameWidth: 500, frameHeight: 300 });
 
     }
 
+
     create() {
-        
+
+        background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0);
+
+
+
+        ermine = this.physics.add.sprite(100, 500, 'ermine').setScale(0.5).setDepth(99);
+
+        //this.anims.create({
+
+        //key: 'ermineAni',
+
+        //frames: this.anims.generateFrameNumbers('ermine',{
+
+        //start: 0,
+
+        //end: 2
+
+        //}),
+
+        //duration: 1000,
+
+        //framerate: 0,
+
+        //repeat: -1
+
+        //ermine.anims.play('ermineAni',true);
+
+        //cursor = this.input.keybord.createCursorKeys();
+
+         //})
+
+
 
     }
 
     update(delta, time) {
-        
+
+
+
+        background.tilePositionX += 2;
+
 
     }
+
 }
+
 export default GameScene;
---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,36 @@
+import Phaser from "phaser";
+
+let buttonPlay;
+
+class MainMenu extends Phaser.Scene { 
+    constructor(test) {
+        super({
+            key: 'MainMenu' 
+        });
+    }
+
+    preload() {
+        this.load.image('play', 'src/image/input/play.png')
+    }
+
+    create() {
+        buttonPlay = this.add.image(250,300, 'play').setScale(0.2);
+
+        buttonPlay.setInteractive();
+
+
+
+        buttonPlay.on('pointerup', ()=>{
+
+            this.scene.start('GameScene');
+
+        })
+    }
+    
+    
+    update() {
+        
+    }
+}
+
+export default MainMenu;  
