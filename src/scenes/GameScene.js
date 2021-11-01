7a9e9d401c8c9c4c0bdcce93fbf301038b58ac42
Nannapatx <nannapat.it@mail.kmutt.ac.th>
2021-11-01 23:02:47 +0700

    Update 1/11/2021 (2)

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -26,13 +26,12 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
-       
 
-        background = this.add.tileSprite(0,0,450,720,'bg').setOrigin(0,0);
+        background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0);
         ermine = this.physics.add.sprite(100, 500, 'ermine').setScale(0.5).setDepth(99);
         this.anims.create({
             key: 'ermineAni',
-            frames: this.anims.generateFrameNumbers('ermine',{
+            frames: this.anims.generateFrameNumbers('ermine', {
                 start: 0,
                 end: 2
             }),
@@ -41,43 +40,41 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         })
 
-        ermine.setCollideWorldBounds(true)  
-        ermine.anims.play('ermineAni',true)
+        ermine.setCollideWorldBounds(true)
+        ermine.anims.play('ermineAni', true)
 
-       
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-        
-    }
 
+    }
     update(delta, time) {
-        
-        if(keyW.isDown){
+
+        if (keyW.isDown) {
 
             ermine.setVelocityY(-500);
 
-        }else if(keyS.isDown){
+        } else if (keyS.isDown) {
 
             ermine.setVelocityY(500);
 
-        }else{
+        } else {
 
             ermine.setVelocityY(0);
 
         }
 
-        if(keyA.isDown){
+        if (keyA.isDown) {
 
             ermine.setVelocityX(-500);
 
-        }else if(keyD.isDown){
+        } else if (keyD.isDown) {
 
             ermine.setVelocityX(500);
 
-        }else{
+        } else {
 
             ermine.setVelocityX(0);
 
@@ -87,4 +84,3 @@ class GameScene extends Phaser.Scene {
     }
 }
 export default GameScene;
-
