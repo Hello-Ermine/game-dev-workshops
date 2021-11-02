76969b78fd08c5e30ee3caa5c6069af1b38b709e
Boss <c3bosskung@gmail.com>
2021-11-02 15:08:40 +0700

    update

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,8 +2,12 @@ import Phaser from "phaser";
 
 let background;
 let ermine;
-let boom;
-let character;
+
+let keyW;
+let keyA;
+let keyS;
+let keyD;
+
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -25,10 +29,6 @@ class GameScene extends Phaser.Scene {
 
         this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png',
             { frameWidth: 500, frameHeight: 300 });
-        this.load.spritesheet('boom', 'src/image/Boom.png',
-            { frameWidth: 105, frameHeight: 99 });
-        this.load.spritesheet('character', 'src/image/character.png',
-            { frameWidth: 51.75, frameHeight: 60.2 });
     }
 
     create() {
@@ -45,49 +45,23 @@ class GameScene extends Phaser.Scene {
         ermine = this.physics.add.sprite(300, 500, 'ermine');
         ermine.setScale(0.4);
 
-        boom = this.physics.add.sprite(100, 450, 'boom');
-        boom.setScale(3);
-
-        character = this.physics.add.sprite(500,450, 'character');
-        character.setScale(3)
-
-
-        this.anims.create({ 
-            key: 'ermineAni', 
-            frames: this.anims.generateFrameNumbers('ermine', { 
-                start: 0, 
-                end: 2 
+        this.anims.create({
+            key: 'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine', {
+                start: 0,
+                end: 2
             }),
-            duration: 400, 
-            framerate: 0,
-            repeat: -1 
-        })
-
-        this.anims.create({ 
-            key: 'boomAni', 
-            frames: this.anims.generateFrameNumbers('boom', { 
-                start: 0, 
-                end: 23
-            }),
-            duration: 383, 
-            framerate: 0,
-            repeat: -1
-        })
-
-        this.anims.create({ 
-            key: 'characterAni', 
-            frames: this.anims.generateFrameNumbers('character', { 
-                start: 0, 
-                end: 79
-            }),
-            duration: 7500, 
+            duration: 400,
             framerate: 0,
             repeat: -1
         })
 
         ermine.anims.play('ermineAni', true);
-        boom.anims.play('boomAni', true);
-        character.anims.play('characterAni', true);
+
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         // ball = this.physics.add.group();
         // ball.create(250, 250, 'football').setScale(0.1);
@@ -98,7 +72,21 @@ class GameScene extends Phaser.Scene {
     }
 
     update(delta, time) {
-        background.tilePositionX += 2;
+        background.tilePositionX += 5;
+        if(keyW.isDown){
+            ermine.setVelocityY(-500);
+        }else if(keyS.isDown){
+            ermine.setVelocityY(500);
+        }else{
+            ermine.setVelocityY(0);
+        }
+        if(keyA.isDown){
+            ermine.setVelocityX(-500);
+        }else if(keyD.isDown){
+            ermine.setVelocityX(500);
+        }else{
+            ermine.setVelocityX(0);
+        }
     }
 }
 export default GameScene;
