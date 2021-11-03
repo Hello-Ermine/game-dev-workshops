9df9cb1c8c09d20a1ba5c40771eea5e356a7372b
Boss <c3bosskung@gmail.com>
2021-11-03 17:34:38 +0700

    Update MainMenu.js

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,5 +1,7 @@
 import Phaser from "phaser";
 
+let buttonPlay;
+
 class MainMenu extends Phaser.Scene {
     constructor(test) {
         super({
@@ -8,11 +10,17 @@ class MainMenu extends Phaser.Scene {
     }
 
     preload() {
-     
+        this.load.image('play', 'src/image/input/play.png');
     }
 
     create() {
-       
+       buttonPlay = this.add.image(650, 350, 'play');
+       buttonPlay.setScale(0.5);
+
+       buttonPlay.setInteractive();
+       buttonPlay.on('pointerup', ()=>{
+           this.scene.start('GameScene');
+       });
     }
 
     update(delta, time) {
