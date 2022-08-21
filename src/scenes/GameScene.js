834b4288ec1627a17e9603dbdb1c459e4fb39b1f
Jemz <102203151+64130500111@users.noreply.github.com>
2022-08-21 14:38:52 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,14 +1,12 @@
 import Phaser from "phaser";
 
-let background;
-let ermine;
-let meow;
+let bg;
+let player;
 let cursor;
 let keyW;
 let keyA;
 let keyS;
 let keyD;
-
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -18,92 +16,64 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-
-        // this.load.image('bg', 'src/image/objectClass/backGround.png');
-        this.load.spritesheet('ermine', 'src/image/objectClass/playerErmine.png', { frameWidth: 500, frameHeight: 300 });
-        this.load.spritesheet('meow', 'src/image/playermeow.jpg', { frameWidth: 500, frameHeight: 300 });
+        this.load.image('bg','../src/image/tileSprite.png');
+        this.load.image('player', '../src/image/playermeow.jpg');
+       
 
     }
 
     create() {
+        bg = this.add.tileSprite(0,0,450,720,'bg');
+        bg.setOrigin(0,0);
+        bg.setScale(2);
 
+        player = this.physics.add.sprite(450,600,'player');
+        player.setScale(0.5);
 
-        ermine = this.physics.add.sprite(100, 500, 'ermine').setScale(0.5).setDepth(99);
-        meow = this.physics.add.sprite(200, 100, 'meow').setScale(0.5);
-        meow.setVelocityY(200);
-
-
-        this.anims.create({
-            key: 'ermineAni',
-            frames: this.anims.generateFrameNumbers('ermine', {
-                start: 0,
-                end: 2
-            }),
-            duration: 1000,
-            framerate: 0,
-            repeat: -1
-        })
-        
-        
-
-        // background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0);
-        ermine.anims.play('ermineAni', true)
-
-        // this.input.on('pointermove',(pointer)=>{
-        //     ermine.x = pointer.x
-        //     ermine.y = pointer.y
+        // this.input.on('pointermove', (pointer)=>{
+        //     player.x = pointer.x
+        //     player.y = pointer.y  //ตั้งให้ตำแหน่งตัวละครไปตามเมาส์ของเรา
         // })
 
-        ermine.setCollideWorldBounds(true)
-        this.physics.add.collider(ermine, meow, () =>{
-            meow.setVisible();
-
-        });
-
         // cursor = this.input.keyboard.createCursorKeys();
-
 
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-
     }
     update(delta, time) {
+        bg.tilePositionY -= 1;
 
-        // background.tilePositionX += 2;
-
-        // if (cursor.up.isDown) {
-        //     ermine.setVelocityY(-500);
-        // } else if (cursor.down.isDown) {
-        //     ermine.setVelocityY(500);
-        // } else {
-        //     ermine.setVelocityY(0);
+        // if(cursor.up.isDown){
+        //     player.setVelocityY(-500);
+        // }else if(cursor.down.isDown){
+        //     player.setVelocityY(500);
+        // }else{
+        //     player.setVelocityY(0);
         // }
-        // if (cursor.left.isDown) {
-        //     ermine.setVelocityX(-500);
-        // } else if (cursor.right.isDown) {
-        //     ermine.setVelocityX(500);
-        // } else {
-        //     ermine.setVelocityX(0);
+        // if(cursor.left.isDown){
+        //     player.setVelocityX(-500);
+        // }else if(cursor.right.isDown){
+        //     player.setVelocityX(500);
+        // }else{
+        //     player.setVelocityX(0);
         // }
 
-        if (keyW.isDown) {
-            ermine.setVelocityY(-500);
-        } else if (keyS.isDown) {
-            ermine.setVelocityY(500);
-        } else {
-            ermine.setVelocityY(0);
+        if(keyW.isDown){
+            player.setVelocityY(-500);
+        }else if(keyS.isDown){
+            player.setVelocityY(500);
+        }else{
+            player.setVelocityY(0);
         }
-        if (keyA.isDown) {
-            ermine.setVelocityX(-500);
-        } else if (keyD.isDown) {
-            ermine.setVelocityX(500);
-        } else {
-            ermine.setVelocityX(0);
+        if(keyA.isDown){
+            player.setVelocityX(-500);
+        }else if(keyD.isDown){
+            player.setVelocityX(500);
+        }else{
+            player.setVelocityX(0);
         }
-
-
 
     }
 }
